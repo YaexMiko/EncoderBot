8b3b9e72f7e1508ad40419e3307719ba2bf2379f
Yae Miko <aloksinghmains756@gmail.com>
2025-03-21 14:24:48 +0530

    Update ffmpeg_utils.py

--- bot/helper/ffmpeg_utils.py
+++ bot/helper/ffmpeg_utils.py
@@ -6,54 +6,53 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
 def get_codec(filepath, channel='v:0'):
+    """
+    Get the codec information for a specific channel (video or audio) in a file.
+    """
     output = check_output(['ffprobe', '-v', 'error', '-select_streams', channel,
                             '-show_entries', 'stream=codec_name,codec_tag_string', '-of', 
                             'default=nokey=1:noprint_wrappers=1', filepath])
     return output.decode('utf-8').split()
 
 def encode(filepath):
+    """
+    Encode a video file to HEVC (x265) format.
+    """
     basefilepath, extension = os.path.splitext(filepath)
     output_filepath = basefilepath + '.[HEVC].mp4'
     assert output_filepath != filepath, "Output filepath should not be the same as input filepath"
     
+    # If the output file already exists, overwrite it
     if os.path.isfile(output_filepath):
         print(f'File "{output_filepath}" already exists, overwriting 🐭')
-        os.remove(output_filepath)  # Remove existing file to overwrite
+        os.remove(output_filepath)
     
     print(f'Processing file: {filepath}')
     
-    # Get the video channel codec
-    video_codec = get_codec(filepath, channel='v:0')
-    if not video_codec:
-        print('Skipping: No video codec reported 🐭')
-        return None
-    
-    # Video transcode options
-    if video_codec[0] == 'hevc' and video_codec[1] == 'hvc1':
-        print('Skipping: Already in HEVC format 🐭')
-        return None
-    elif video_codec[0] == 'hevc':
-        # Copy stream to hvc1
-        video_opts = '-c:v copy -tag:v hvc1'
-    else:
-        # Transcode to h265 / hvc1 with faster settings
-        video_opts = '-c:v libx265 -crf 28 -tag:v hvc1 -preset ultrafast -threads 8'
+    # Always encode to HEVC, even if the video is already in HEVC format
+    video_opts = '-c:v libx265 -crf 28 -tag:v hvc1 -preset ultrafast -threads 8'
     
     # Get the audio channel codec
     audio_codec = get_codec(filepath, channel='a:0')
     if not audio_codec:
-        audio_opts = ''
+        audio_opts = ''  # No audio stream
     elif audio_codec[0] == 'aac':
-        audio_opts = '-c:a copy'
+        audio_opts = '-c:a copy'  # Copy AAC audio stream
     else:
-        audio_opts = '-c:a aac -b:a 128k'
+        audio_opts = '-c:a aac -b:a 128k'  # Transcode non-AAC audio to AAC
     
-    # Run FFMPEG command
+    # Run FFMPEG command to encode the video
     call(['ffmpeg', '-i', filepath] + video_opts.split() + audio_opts.split() + [output_filepath])
-    os.remove(filepath)  # Remove the original file after encoding
+    
+    # Remove the original file after encoding
+    os.remove(filepath)
+    
     return output_filepath
 
 def get_thumbnail(in_filename, path, ttl):
+    """
+    Generate a thumbnail from a video file at a specific time.
+    """
     out_filename = os.path.join(path, str(time.time()) + ".jpg")
     open(out_filename, 'a').close()
     try:
@@ -70,13 +69,19 @@ def get_thumbnail(in_filename, path, ttl):
         return None
 
 def get_duration(filepath):
+    """
+    Get the duration of a video file in seconds.
+    """
     metadata = extractMetadata(createParser(filepath))
     if metadata and metadata.has("duration"):
         return metadata.get('duration').seconds
     return 0
 
 def get_width_height(filepath):
+    """
+    Get the width and height of a video file.
+    """
     metadata = extractMetadata(createParser(filepath))
     if metadata and metadata.has("width") and metadata.has("height"):
         return metadata.get("width"), metadata.get("height")
-    return 1280, 720
+    return 1280, 720  # Default resolution if metadata is not available
